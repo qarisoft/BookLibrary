c0756e26f46064c30fa67975870f2ee4c1aa5f37
salah <cloud.data.sa@gmail.com>
2025-02-05 02:14:24 +0300

    sas

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,7 +1,45 @@
+"use client"
+import { BackendUrl } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { Button } from "@/components/ui/button";
+interface Book {
+  id: number,
+  bookName: string,
+  authorName: string,
+  price: number,
+  image: string | undefined,
+  genreId: number,
+  genre: string | undefined
 
+}
 export default function Home() {
+  const [books, setBooks] = useState<Book[]>([])
+  useEffect(() => {
+    populateData()
+  }, [])
+
+  async function populateData() {
+    const response = await fetch(`${BackendUrl}/api/books`);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      setBooks(data)
+
+      // setForecasts(data);
+    }
+  }
   return (
 
 
@@ -10,7 +48,7 @@ export default function Home() {
 
         <div className="flex flex-1 justify-center gap-8">
           <Link href={'/'} className="p-2">Home</Link>
-          <Link href={'/'} className="p-2">Dashboard</Link>
+          <Link href={'/dashboard'} className="p-2">Dashboard</Link>
         </div>
         <div className="flex gap-4 px-4 py-2">
           <Link href={'/login'}>login</Link>
@@ -21,12 +59,79 @@ export default function Home() {
       <section className=" flex justify-center">
 
         <div className=" max-w-6xl flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-5">
-            {Array.from({ length: 20 }).map((_, i) => (
-              <div key={i} className="m-2 aspect-square rounded-xl bg-muted/50 text-center align-middle  flex justify-center items-center" >
-                <div className="">
-                  {i}
+          <div className="grid auto-rows-min gap-4 md:grid-cols-4">
+            {books.map((b, i) => (
+              <div key={i} className="m-2 aspect-square   flex flex-col " >
+
+
+                {/*  */}
+                <div className="flex-1 bg-muted/50 rounded-xl">
+
                 </div>
+
+                {/*  */}
+                <div className="pb-1"></div>
+
+
+
+                {/*  */}
+                <section className="">
+                  <Drawer>
+                    <DrawerTrigger>
+                      <div className="">{b.bookName}</div>
+
+                    </DrawerTrigger>
+                    <DrawerContent >
+                      <div className="flex justify">
+
+                        <div className="h-[30rem] flex-1 aspect-squar rounded-lg m-1 bg-slate-300"></div>
+                        <div className="max-w-4xl">
+
+                          <DrawerHeader className="min-w-[25rem] h-full flex flex-col ">
+                            <section className=" flex-1">
+
+                              <DrawerTitle className="py-2">{b.bookName}</DrawerTitle>
+                              
+                              <DrawerDescription className="px-2">
+                                <p >
+                                  this is book Description.
+                                </p>
+                                <p className="text-wrap">................. .................. ................. ................ ................ </p>
+                                <p className="text-wrap">................. .................. ................. ................ ................ </p>
+                                <p className="text-wrap">................. .................. ................. ................ ................ </p>
+                                <p className="text-wrap">................. .................. ................. ................ ................ </p>
+
+                              </DrawerDescription>
+
+                              <div className="p-2">
+
+                                <div className="text-green-800">{b.genre}</div>
+                                <div className="h-1"></div>
+                                <div className="flex justify-aroun">
+
+                                </div>
+                              </div>
+                            </section>
+                            <Button>Buy
+                              <span>
+                                ${b.price}
+                              </span>
+                            </Button>
+                          </DrawerHeader>
+
+                        </div>
+                      </div>
+                    </DrawerContent>
+                  </Drawer>
+
+                  <div className="p-0.5"></div>
+                  <div className="flex gap-4">
+
+                    <div className="text-xs">{b.genre}</div>
+                    <div className="text-xs">${b.price}</div>
+                  </div>
+                </section>
+
 
               </div>
             ))}
